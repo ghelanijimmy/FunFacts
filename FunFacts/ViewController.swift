47fd6818201e602fe5c5ea5bf0d89350fc843169
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2018-03-19 21:30:39 -0400

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Jimmy Ghelani on 2018-03-19.
+//  Copyright © 2018 JGDesign Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
